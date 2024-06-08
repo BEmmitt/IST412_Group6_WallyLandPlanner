2405b17e91499a80fdff441b1f04ba789bba2689
BEmmitt <124312303+BEmmitt@users.noreply.github.com>
2024-06-08 18:55:16 -0400

    Add files via upload

---
+++ Group6WallyLandPlanner/src/main/java/com/mycompany/group6wallylandplanner/Group6WallyLandPlanner.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.group6wallylandplanner;
+
+/**
+ *
+ * @author bemmi
+ */
+public class Group6WallyLandPlanner {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
