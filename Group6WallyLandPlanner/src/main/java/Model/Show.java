cc090827f0bc83ce0ba3779f96634f8e814545e1
Jordan Joe Runyon <113555067+jjr6639@users.noreply.github.com>
2024-06-09 18:52:11 -0400

    user and show with javadocs

--- Group6WallyLandPlanner/src/main/java/Model/Show.java
+++ Group6WallyLandPlanner/src/main/java/Model/Show.java
@@ -4,41 +4,73 @@
  */
 package Model;
 
-/**
- *
- * @author Jordan
- */
-
 import java.util.Date;
 
+/**
+ * Represents a show with a name, date, and time.
+ * Provides methods to get and set these attributes.
+ * 
+ * @author Jordan
+ */
 public class Show {
     
     private String name;
     private Date date;
     private Date time;
 
+    /**
+     * Gets the name of the show.
+     * 
+     * @return the name of the show
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the show.
+     * 
+     * @param name the new name of the show
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the date of the show.
+     * 
+     * @return the date of the show
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * Sets the date of the show.
+     * 
+     * @param date the new date of the show
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * Gets the time of the show.
+     * 
+     * @return the time of the show
+     */
     public Date getTime() {
         return time;
     }
 
+    /**
+     * Sets the time of the show.
+     * 
+     * @param time the new time of the show
+     */
     public void setTime(Date time) {
         this.time = time;
     }
 }
+
 
--- Group6WallyLandPlanner/src/main/java/Model/User.java
+++ Group6WallyLandPlanner/src/main/java/Model/User.java
@@ -4,34 +4,71 @@
  */
 package Model;
 
+/**
+ * Represents a user with a username, email, and password.
+ * Provides methods to get and set these attributes.
+ * 
+ * @author Jordan
+ */
 public class User {
 
     private String username;
     private String email;
     private String password;
 
+    /**
+     * Gets the username of the user.
+     * 
+     * @return the username of the user
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets the username of the user.
+     * 
+     * @param username the new username of the user
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets the email of the user.
+     * 
+     * @return the email of the user
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets the email of the user.
+     * 
+     * @param email the new email of the user
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Gets the password of the user.
+     * 
+     * @return the password of the user
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the password of the user.
+     * 
+     * @param password the new password of the user
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 }
+
 
