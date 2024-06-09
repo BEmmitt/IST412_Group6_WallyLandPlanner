f04cc61bf76795d892ef6784fee01f6d2270a0ad
Paul Michalek <pbm5434@psu.edu>
2024-06-09 18:11:45 -0400

    Itinerary done, need javadocs and Reservation to be set

--- Group6WallyLandPlanner/src/main/java/Model/Itinerary.java
+++ Group6WallyLandPlanner/src/main/java/Model/Itinerary.java
@@ -3,11 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Model;
+import java.util.List;
 
 /**
  *
  * @author pault
  */
 public class Itinerary {
+    
+    private User user;
+    private List<Reservation> Reservations;
+    
+    public User getUser()
+    {
+        return user;
+    }
+    public void setUser(User user)
+    {
+        this.user=user;
+    }
+    
+    public List<Reservation> getReservations()
+    {
+        return reservations;
+    }
+    
+    public void setReservations(List<Reservation> reservations)
+    {
+        this.reservations=reservations;
+    }
+            
     
 }
