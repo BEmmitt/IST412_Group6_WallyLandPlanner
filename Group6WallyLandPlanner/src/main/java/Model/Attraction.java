1b34d702de418a083846b1735a6408ab623f66d0
Paul Michalek <pbm5434@psu.edu>
2024-06-09 18:15:01 -0400

    Attraction done, need javadocs and name

--- Group6WallyLandPlanner/src/main/java/Model/Attraction.java
+++ Group6WallyLandPlanner/src/main/java/Model/Attraction.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Model;
+import java.util.Date;
 
 /**
  *
@@ -10,4 +11,37 @@ package Model;
  */
 public class Attraction {
     
+    private String name;
+    private int capacity;
+    private Date timeSlot;
+    
+    public String getName;
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    
+    public int getCapacity()
+    {
+        return capacity;
+    }
+    
+    public void setCapacity(int Capacity)
+    {
+        this.capacity=capacity;
+    }
+    
+    public Date getTimeSlot()
+    {
+        return timeSlot;
+    }
+    
+    public void setTimeSlot(Date timeSlot)
+    {
+        this.timeSlot=timeSlot;
+    }
 }
