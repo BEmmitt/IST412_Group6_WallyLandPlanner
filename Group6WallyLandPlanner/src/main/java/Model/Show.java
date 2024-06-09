267ad5d5bb674b1388a88cf4f201d4f5980484c4
Jordan Joe Runyon <113555067+jjr6639@users.noreply.github.com>
2024-06-09 18:43:43 -0400

    user and show done, need to add javadocs

--- Group6WallyLandPlanner/src/main/java/Model/Show.java
+++ Group6WallyLandPlanner/src/main/java/Model/Show.java
@@ -8,6 +8,37 @@ package Model;
  *
  * @author Jordan
  */
+
+import java.util.Date;
+
 public class Show {
     
+    private String name;
+    private Date date;
+    private Date time;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
 }
+
