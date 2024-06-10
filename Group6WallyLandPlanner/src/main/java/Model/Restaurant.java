1c9b5ea005f070722fb8c193859b516a59fa4465
prajwal <pmt5233@psu.edu>
2024-06-09 20:29:16 -0400

    Restaurant Commit

--- Group6WallyLandPlanner/src/main/java/Model/Restaurant.java
+++ Group6WallyLandPlanner/src/main/java/Model/Restaurant.java
@@ -4,10 +4,88 @@
  */
 package Model;
 
+import java.util.List;
+
 /**
  *
  * @author Praj
  */
 public class Restaurant {
     
+    private String name;
+    private String address;
+    private List<String> menu;
+    private double rating;
+    
+    /**
+     * Gets the name of the restaurant.
+     * 
+     * @return the name of the restaurant.
+     */
+    public String getName() {
+        return name;
+    }
+    
+    /**
+     * Sets the name of the restaurant.
+     * 
+     * @param name the name to set for the restaurant.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    /**
+     * Gets the address of the restaurant.
+     * 
+     * @return the address of the restaurant.
+     */
+    public String getAddress() {
+        return address;
+    }
+    
+    /**
+     * Sets the address of the restaurant.
+     * 
+     * @param address the address to set for the restaurant.
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    /**
+     * Gets the menu of the restaurant.
+     * 
+     * @return the menu of the restaurant.
+     */
+    public List<String> getMenu() {
+        return menu;
+    }
+    
+    /**
+     * Sets the menu of the restaurant.
+     * 
+     * @param menu the menu to set for the restaurant.
+     */
+    public void setMenu(List<String> menu) {
+        this.menu = menu;
+    }
+    
+    /**
+     * Gets the rating of the restaurant.
+     * 
+     * @return the rating of the restaurant.
+     */
+    public double getRating() {
+        return rating;
+    }
+    
+    /**
+     * Sets the rating of the restaurant.
+     * 
+     * @param rating the rating to set for the restaurant.
+     */
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
